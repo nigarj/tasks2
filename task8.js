7e78c3929a5db689814de0eb3ab02f7d070a5711
nigarj <96123286+nigarj@users.noreply.github.com>
2022-03-25 00:08:36 +0400

    Update task8.js

--- task8.js
+++ task8.js
@@ -1,12 +1,12 @@
-let a=['hello','world','what','why']
+let a1=['hello','world','what','why']
 let b='w'
 let c=[]
-function method(a,b) {
-    for (let i=0;i<a.length;i++){
-        if(a[i].charAt(0)==b) {
-            c.push(a[i])
+function method(a1,b) {
+    for (let i=0;i<a1.length;i++){
+        if(a1[i].charAt(0)==b) {
+            c.push(a1[i])
         }
     }
     console.log(c)
 }
-method(a,b)
+method(a1,b)
