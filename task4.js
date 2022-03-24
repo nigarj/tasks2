b4349342cc26a2daab37fe5d98d132820df03b39
nigarj <96123286+nigarj@users.noreply.github.com>
2022-03-25 00:04:37 +0400

    Update task4.js

--- task4.js
+++ task4.js
@@ -1,12 +1,12 @@
 let arr3=[100,205,300];
 let major=[];
-let sum=0;
+let sum1=0;
 function mArr(a) {
     for (let i=0; i<a.length; i++) {
-        sum=sum+a[i];
+        sum1=sum1+a[i];
     }
     for (let x=0; x<a.length; x++ ){
-        if (a[x]>sum/3){
+        if (a[x]>sum1/3){
             major.push(a[x])
         }
     }
