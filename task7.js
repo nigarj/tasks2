3d590648e4eaa1cd8e5bfe6082ae371e4fee1284
nigarj <96123286+nigarj@users.noreply.github.com>
2022-03-25 00:06:22 +0400

    Update task7.js

--- task7.js
+++ task7.js
@@ -1,10 +1,10 @@
 let a=[12,13,15,16,17]
-let b=[10,11,12,13,14]
+let b1=[10,11,12,13,14]
 let d=[]
-function nArr(a,b) {
+function nArr(a,b1) {
     for (let i=0; i<a.length; i++){
-        for (let x=0; x<b.length; x++) {
-            if (a[i]===b[x]) {
+        for (let x=0; x<b1.length; x++) {
+            if (a[i]===b1[x]) {
                 d.push(a[i])
 
             }
@@ -12,4 +12,4 @@ function nArr(a,b) {
     }
     console.log(d)
 }
-nArr(a,b)
+nArr(a,b1)
